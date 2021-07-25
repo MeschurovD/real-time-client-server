243e5ba2b14cd9d8d58c3d7267f9b6156c620cd7
djoker255 <meschuro4@gmail.com>
2021-07-25 15:17:00 +0500

    First commit

---
+++ server/longpulling.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const cors = require('cors')
+
+const PORT = 5000
+
+const app = express()
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`))
